2979347cd708a2631dd3b5c96ec47ea85721fb8c
Wagner José Florencio Junior <wagner.junior@live.com>
2017-05-05 17:58:29 -0300

    Criação do layout

--- 4-Implementacao/4.1-Codigo_Fonte/Finis/App_Start/BundleConfig.cs
+++ 4-Implementacao/4.1-Codigo_Fonte/Finis/App_Start/BundleConfig.cs
@@ -14,18 +14,20 @@ namespace Finis
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                        "~/Scripts/materialize/materialize.js"));
+
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      //"~/Content/materialize/css/materialize.css",
+                      ));
         }
     }
 }
