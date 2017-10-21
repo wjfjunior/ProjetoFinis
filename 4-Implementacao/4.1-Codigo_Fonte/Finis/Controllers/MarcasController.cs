1bd61c04e673bc9f02de679370a44ee539069061
Wagner J. Florencio Jr <wagner.junior@live.com>
2017-10-21 13:08:39 -0200

    Alterações documento e cód

---
+++ 4-Implementacao/4.1-Codigo_Fonte/Finis/Controllers/MarcasController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Finis.DAL;
+using Finis.Models;
+
+namespace Finis.Controllers
+{
+    public class MarcasController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Marcas
+        public ActionResult Index()
+        {
+            return View(db.Marca.ToList().OrderBy(m => m.nome));
+        }
+
+        [HttpPost]
+        public ActionResult Index(string pesquisar)
+        {
+            return View("Index", db.Marca.Where(c => c.nome.Contains(pesquisar)).ToList());
+        }
+
+        // GET: Marcas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marca marca = db.Marca.Find(id);
+            if (marca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marca);
+        }
+
+        // GET: Clientes/Details/5
+        public JsonResult Detalhes(int? id)
+        {
+            bool sucesso;
+            string resultado;
+
+            if (id == null)
+            {
+                sucesso = false;
+                resultado = "Não encontrado!";
+            }
+            else
+            {
+                Marca marca = db.Marca.Find(id);
+                if (marca == null)
+                {
+                    sucesso = false;
+                    resultado = "Não encontrado!";
+                }
+                else
+                {
+                    sucesso = true;
+                    resultado = marca.Serializar();
+                }
+            }
+            var obj = new
+            {
+                Sucesso = sucesso,
+                Resultado = resultado
+            };
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Marcas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        private bool VerificaJaExiste(Marca marca)
+        {
+            List<Marca> resultado = new List<Marca>();
+
+            resultado = db.Marca.Where(p => p.nome == marca.nome).ToList();
+            if (resultado.Count() > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // POST: Marcas/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Marca model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.nome = model.nome.ToUpper();
+                if (VerificaJaExiste(model))
+                {
+                    ViewBag.Erro = "Ja existe um registro com os valores informados!";
+                    return View(model);
+                }
+                db.Marca.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        // GET: Marcas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marca marca = db.Marca.Find(id);
+            if (marca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marca);
+        }
+
+        // POST: Marcas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                marca.nome = marca.nome.ToUpper();
+                db.Entry(marca).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(marca);
+        }
+
+        // GET: Marcas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marca marca = db.Marca.Find(id);
+            if (marca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marca);
+        }
+
+        // POST: Marcas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Marca marca = db.Marca.Find(id);
+            db.Marca.Remove(marca);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Clientes/Delete/5
+        public JsonResult DeletarRegistro(int? id)
+        {
+            if (id != null)
+            {
+                Marca model = db.Marca.Find(id);
+                db.Marca.Remove(model);
+                db.SaveChanges();
+            }
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
