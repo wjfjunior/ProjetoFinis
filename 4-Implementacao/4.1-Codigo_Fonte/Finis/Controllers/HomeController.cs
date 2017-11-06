6be78b6b1ce5bde129a1a746c352ccae98bdb6c3
Wagner J. Florencio Jr <wagner.junior@live.com>
2017-11-06 01:51:45 -0200

    Criação do dashboard

--- 4-Implementacao/4.1-Codigo_Fonte/Finis/Controllers/HomeController.cs
+++ 4-Implementacao/4.1-Codigo_Fonte/Finis/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Finis.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,22 @@ namespace Finis.Controllers
     [Authorize(Roles = "Administrador, Funcionário")]
     public class HomeController : Controller
     {
+        private Contexto db = new Contexto();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult TotalClientes()
+        {
+            int totClientes = db.Cliente.Count();
+
+            var obj = new
+            {
+                Total = totClientes.ToString()
+            };
+            return Json(obj, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ 4-Implementacao/4.1-Codigo_Fonte/Finis/Scripts/Graficos.js
@@ -0,0 +1,68 @@
+﻿google.charts.load('current', { 'packages': ['bar'] });
+google.charts.setOnLoadCallback(drawChartBarras);
+
+function drawChartBarras() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+    ]);
+
+    var options = {
+        chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        }
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChartRosca);
+
+function drawChartRosca() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChartPizza);
+
+function drawChartPizza() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
